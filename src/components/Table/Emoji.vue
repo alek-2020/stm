772237ae3a73a74201ff3022743062bd26e0d5ac
alek-2020 <alek-2020@yandex.ru>
2018-07-14 23:29:42 +0300

    настроил компонент с emoji

--- src/components/Table/Emoji.vue
+++ src/components/Table/Emoji.vue
@@ -1,10 +1,56 @@
+// TODO: При добавлении задачи появляется скролл, нужно добавить встроенный пересчет высоты vueperfectscrollbar
+
 <template>
-    <img class="emoji__one" src="../../../img/emoji/setOne/smile.svg" alt="">
+    <div class="emoji__box">
+        <div 
+        v-for="(oneEmoji, index) in listOfEmoji"
+        @click="cnangeEmoji(index)"
+        :key="oneEmoji.name"
+        class="emoji__one" 
+        v-html="oneEmoji.svg">
+        </div>
+    </div>
 </template>
 
+<script>
+import { svgEmoji } from "../../OtherSrc/svg.js";
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+
+    computed: {
+        listOfEmoji() {
+            return svgEmoji;
+        }
+    },
+    methods: {
+      cnangeEmoji(index) {
+          console.log(index);
+      }
+    },
+    mounted() {
+        console.log('Получили emoji', this.listOfEmoji);
+    }
+}
+</script>
+
+
 <style lang="scss">
-   .emoji__one {
-       height: 30px;
-       width: auto;
+   .emoji {
+       &__one {
+        height: 30px;
+        width: 30px;
+
+         &:not(:first-child) {
+            margin-left: 5px;
+         }
+       }
+        &__box {
+         display: flex;
+       }
    }
 </style>
