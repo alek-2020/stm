16181b4df3faad05445803b5591d11433d8f96bc
Shugaev <alek-2020@yande.ru>
2018-09-10 18:33:43 +0300

    .

--- src/store/modules/newTaskList.js
+++ src/store/modules/newTaskList.js
@@ -3,179 +3,93 @@ import * as firebase from "firebase";
 export default {
   actions: {
 
-    /////////////////////////////////////////////////
-    // УПРАВЛЯЮЩАЯ ФУНКЦИЯ ДОБАВЛЕНИЯ НОВОГО СПИСКА
-    //////////////////////////////////////////////////
-
+    // УПРАВЛЯЮЩАЯ ФУНКЦИЯ
     addTaskList({
       dispatch
     }) {
-      // 1. Формируем параметры нового листа задач и пушим в TaskLists
-      dispatch('addList')
-        .then(response => {
-          console.log('newTaskList. Запушили список в локальный массив и в taskLists', response);
-          const tableId = response.tableId;
-          const newListId = response.newListId;
-
-          // 2 Запушим в список его id
-          dispatch('pushKeyToTaskList', newListId);
-          // 3. Отправим обновленные списки на сервер
-          return dispatch('addListIdToTable', {
-            tableId,
-            newListId
-          });
-        }).then(response => {
-          // console.log();
-        }).catch(error => {
-          console.log('Полный провал. Ошибка: ', error);
+      // 1. Формируем параметры нового листа
+      dispatch('makeListObj')
+        .then(newTaskList => {
+          // 2. Пушим параметры
+          return dispatch('pushListObj', newTaskList)
         })
-    },
-
-
-
-    //добавляем лист задач
-    addList({
-      dispatch,
-      commit,
-      state,
-      rootState
-    }) {
-      return new Promise((resolve, reject) => {
-
-        let tableInd = rootState.activeTableIndex;
-        console.log(rootState.allTasks[tableInd])
-        let TableLists = rootState.allTasks[tableInd].taskLists;
-        const tableId = rootState.allTasks[tableInd].id;
-        const lastTaskInd = rootState.allTasks[rootState.activeTableIndex].taskLists.length - 1;
-        const userId = rootState.userId;
-
-        //Получим уникальный id списка
-        let newListIndex = 0;
-        //Получим id последнего cписка, если списков нет - оставим 0
-        if (rootState.allTasks[rootState.activeTableIndex].taskLists.length > 0) {
-          newListIndex = rootState.allTasks[rootState.activeTableIndex].taskLists[lastTaskInd].listIndex + 1
-        }
-
-        // console.log('.newTaskList. ', lastTaskInd, rootState.activeTableIndex, rootState.allTasks[rootState.activeTableIndex].taskLists.length, newListIndex);
-        //Формируем параметры нового списка
-        const newTaskList = {
-          name: '',
-          color: 1,
-          tasks: [],
-          listIndex: newListIndex,
-          userId,
-          tableId
-        }
-
-        //если мы ещё не создавали списков, то будет undefined
-        //Так что делаем проверку
-        if (typeof TableLists == 'undefined') {
-          rootState.allTasks[tableInd].taskLists = [newTaskList]
-          // console.log('добавили в массив ', rootState.allTasks[tableInd].taskLists);
-        } else {
-          rootState.allTasks[tableInd].taskLists.push(newTaskList);
-        }
-
-        firebase
-          .database()
-          .ref("taskLists/")
-          .push(newTaskList)
-          .then(data => {
-            //теперь привяжем id списка к столу
-            const newListId = data.key;
-            console.log('.newTaskList. data.key ', newListId, data.key);
-            // докинем наш ключ в  allTasks. Сначала нужно узнать его индекс
-            const ind = rootState.allTasks[tableInd].taskLists.length - 1;
-            rootState.allTasks[tableInd].taskLists[ind].id = newListId;
-            // console.log('.newtasklist наш лист с id',  rootState.allTasks[tableInd].taskLists[ind].id)
-
-            // dispatch('getTaskListInTable', { tableId, newListId });
-            resolve({
-              tableId,
-              newListId
-            });
-
-          })
-          .catch(error => {
-            console.log('Полный провал. Ошибка: ', error);
-          })
-      })
-    },
-
-    pushKeyToTaskList({
-      dispatch,
-      commit,
-      state,
-      rootState
-    }, listKey) {
-      // console.log('k ',listKey);
-      firebase
-        .database()
-        .ref("taskLists/" + listKey + "/id")
-        .set(listKey)
-        .then(data => {
-          //    console.log('.newTaskList. запушили в список его id ', data);
+        .then(newListId => {
+          // 3. Пушим id списка в него же
+          dispatch('pushKeyToTaskList', newListId);
         })
         .catch(error => {
           console.log('Полный провал. Ошибка: ', error);
         })
     },
 
-    getTaskListInTable({
-      dispatch,
-      commit,
-      state,
+    //добавляем лист задач
+    makeListObj({
       rootState
-    }, {
-      tableId,
-      newListId
     }) {
       return new Promise((resolve, reject) => {
 
-        firebase
-          .database()
-          .ref("tables/" + tableId + "/taskLists")
-          .once('value')
-          .then(data => {
+        let tableInd = rootState.activeTableIndex;
+        let TableLists = rootState.allTasks[tableInd].taskLists;
+        const tableId = rootState.allTasks[tableInd].id;
+        const lastTaskInd = rootState.allTasks[rootState.activeTableIndex].taskLists.length - 1;
+        const userId = rootState.userId;
 
-            const taskListsId = data.val();
-            resolve({
-              tableId,
-              newListId,
-              taskListsId
-            });
-          })
-          .catch(error => {
-            console.log('Полный провал. Ошибка: ', error);
-          })
+        //Получим уникальный id списка
+        let newListIndex = TableLists.length > 0 ? TableLists[lastTaskInd].listIndex + 1 : 0
 
+        //Формируем параметры нового списка
+        const newTaskList = {
+          name: '',
+          tasks: [],
+          listIndex: newListIndex,
+          color: 0,
+          userId,
+          tableId
+        }
+
+        resolve(newTaskList)
       })
     },
 
-    addListIdToTable({
-      dispatch,
-      commit,
-      state,
+    // ПУШИМ ОБЪЕКТ НА СЕРВЕР
+    pushListObj({
       rootState
-    }, {
-      tableId,
-      newListId
-    }) {
+    }, newTaskList) {
       return new Promise((resolve, reject) => {
+        let TableLists = rootState.allTasks[rootState.activeTableIndex].taskLists;
 
         firebase
           .database()
-          .ref("tables/" + tableId + "/taskLists/" + newListId)
-          .set(newListId)
+          .ref("taskLists/")
+          .push(newTaskList)
           .then(data => {
+            const newListId = data.key
+            // докинем наш ключ в  allTasks
+            newTaskList.id = newListId;
 
-            resolve('newTaskList. Список запушен. Все ОК!');
+            // Пушим в старый массив(если есть) или создаем новый
+            !TableLists ? TableLists = [newTaskList] : TableLists.push(newTaskList);
+
+            resolve(newListId);
+
           })
           .catch(error => {
             console.log('Полный провал. Ошибка: ', error);
+            reject('Ошибка добавления задачи', error)
           })
       })
-
     },
+
+    // ДОПУШИВАЕМ ID СПИСКА В НЕГО ЖЕ
+    pushKeyToTaskList({}, listKey) {
+      firebase
+        .database()
+        .ref("taskLists/" + listKey + "/id")
+        .set(listKey)
+        .catch(error => {
+          console.log('Полный провал. Ошибка: ', error);
+        })
+    },
+
   }
 }
